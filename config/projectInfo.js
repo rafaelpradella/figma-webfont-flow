8d89f0a4c2246e2210f94f5208955b2ccc2fa13c
Rafael Pradella <pradella@outlook.com>
2018-05-30 18:27:52 -0300

    Scaffolding code and requirements

---
+++ config/projectInfo.js
@@ -0,0 +1,8 @@
+﻿const project = {
+    /* Copy the URL's ID of your Figma file */
+    fileID: "<FILE_ID>",
+    /* Create a Figma Project Token (Menu => Account Settings => Personal Access Tokens) then insert here */
+	projectToken : "<PROJECT_TOKEN>"
+}
+
+module.exports = project;
---
+++ index.js
@@ -0,0 +1,5 @@
+﻿const webfont = require("webfont");
+const request = require("request");
+const project = require("./config/projectInfo.js");
+const api = 'https://api.figma.com/v1';
+
